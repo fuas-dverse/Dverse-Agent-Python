4fb45ed70545f438b10893f91e99513e17b96705
Arenco Meevissen <479450@student.fontys.nl>
2024-06-05 10:50:25 +0200

    Add better instructions. With longer description

--- setup.py
+++ setup.py
@@ -14,9 +14,12 @@ setup(
         ],
     },
     author="Fontys-Dverse",
-    description="A package for creating an Agent for the DVerse platform."
+    description="A package for creating an Agent for the DVerse platform.",
+    long_description="A package for creating an Agent for the DVerse platform. You can see an example at https://github.com/fuas-dverse/dverse-agent-python/blob/main/demo_agent.py for how you can create your own."
 )
 
-#in the dverse-agent-python
-# first, run : pip install .
-# then execute : python startup.py sdist to create a tar.gz. file of the package
+# In the dverse-agent-python repository 
+# Make sure that you do not cd away from the base directory of the repository for the next few steps.
+# First, run : pip install . # This will clone the repository and create a package out of it.
+# Then execute : python setup.py sdist # This will create a tar.gz. file of the package.
+# You can find the tar file in the dist folder in the same repository as where you are now. 
